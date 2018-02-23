fa3d6f925f9b6580030e431985df19267bc006f4
CUCHOT Florian <fcuchot@axones.com>
2018-02-23 17:06:14 +0100

    Working (2) on JSON Serialize_zoo

--- ZooRunner/ZooSample.Tests/ZooSample.Tests.cs
+++ ZooRunner/ZooSample.Tests/ZooSample.Tests.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using FluentAssertions;
 using ZooSample;
 using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 namespace ZooSample.Tests
 {
@@ -36,9 +37,47 @@ namespace ZooSample.Tests
             z.Find<Cat>( "Minnet" ).Should().NotBeNull();
             z.Find<Bird>( "Titi" ).Should().NotBeNull();
        
-            JObject o = JObject.FromObject( z.Find<Animal>("Titi" ));
+            JObject o = JObject.FromObject( z.Find<Bird>("Titi" ));
+         
 
             var deserialzation = z.Read( o );
+
+
+            var allAnimals = z.SelectAll<List<KeyValuePair<string, Animal>>>();
+
+           var output = Newtonsoft.Json.JsonConvert.SerializeObject( allAnimals );
+        //JArray array  = JArray.Parse( output );
+            
+        //        JObject o1 = JObject.Parse( array[1].ToString() );
+        //    //o1["Animals"] = JObject.FromObject( allAnimals );
+
+
+   
+        }
+        [Test]
+        public void PetConverter()
+        {
+            Zoo z = new Zoo();
+            z.CreateBird( "Titi" );
+           
+
+            var json = JsonConvert.SerializeObject( z.Find<Bird>( "Titi" ) );
+
+            var converter = new Zoo.PetConverter();
+            var deserializedArray = JsonConvert.DeserializeObject<Animal>(
+                json,
+                converter );
+
+            
+
+            //for( var i = 0; i < originalArray.Length; i++ )
+            //{
+            //    var original = originalArray[i];
+            //    var deserialized = deserializedArray[i];
+
+            //    Assert.Equal( original.GetType(), deserialized.GetType() );
+            //    Assert.Equal( original.Name, deserialized.Name );
+            //}
         }
     }
 }
--- ZooRunner/ZooSample/Zoo.cs
+++ ZooRunner/ZooSample/Zoo.cs
@@ -1,8 +1,11 @@
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Drawing;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -104,21 +107,109 @@ namespace ZooSample
         {
             throw new NotImplementedException();
         }
-
+        public T SelectAll<T>(  ) where T : List<KeyValuePair<string, Animal>>
+        {
+            var list = new List<KeyValuePair<string, Animal>>();
+           
+            var a = _animals.ToList();
+            foreach(var z in a )
+            {
+                list.Add( z );
+            }
+            return list as T;
+        }
         public Animal Read( JObject o )
         {
+
+            string typeName = (o["Type"]).ToString();
+            switch( typeName )
+            {
+                case "Bird":
+                    
+                   
+                   Bird b2 =  o.ToObject<Bird>();
+                    return b2;
+                case "Cat":
+                    return new Cat( this, o["Name"].ToString() );
+                default:
+                    return null;
+            }
+
+            var a = (Bird)System.Reflection.Assembly.GetExecutingAssembly().CreateInstance("Bird" );
+            var arr = new object[] { this, "tott" };
+            Animal b = (Animal)Activator.CreateInstance(typeof(Bird), arr);
             
+            //_animals[obj.Name] = obj;
+
+
 
             List<string> keys = o.Properties().Select( p => p.Name ).ToList();
             List<JToken> values = new List<JToken>();
 
+            var props = typeof( Animal ).GetProperties();
+            
             foreach( string k in keys )
             {
-                var value = o.GetValue( k );
-                values.Add( value );
+               
+              props.Where( p => p.Name == k ).FirstOrDefault().SetValue(b, o.GetValue(k));
+                
+
+                //var value = o.GetValue( k );
+                //values.Add( value );
             }
             
             return null;
         }
+        public abstract class AbstractJsonConverter<T> : JsonConverter
+        {
+            protected abstract T Create( Type objectType, JObject jObject );
+
+            public override bool CanConvert( Type objectType )
+            {
+                return typeof( T ).IsAssignableFrom( objectType );
+            }
+
+            public override object ReadJson(
+                JsonReader reader,
+                Type objectType,
+                object existingValue,
+                JsonSerializer serializer )
+            {
+                var jObject = JObject.Load( reader );
+
+                T target = Create( objectType, jObject );
+                serializer.Populate( jObject.CreateReader(), target );
+
+                return target;
+            }
+
+            public override void WriteJson(
+                JsonWriter writer,
+                object value,
+                JsonSerializer serializer )
+            {
+                throw new NotImplementedException();
+            }
+
+            protected static string TypeOfAnimal(JObject jObject, string name)
+            {          
+                return jObject.GetValue( name ).ToString();
+            }
+        }
+
+        public class PetConverter : AbstractJsonConverter<Animal>
+        {
+            protected override Animal Create( Type objectType, JObject jObject )
+            {
+                if( TypeOfAnimal( jObject, "Type" ) == "Bird")
+                    return null;
+                if( TypeOfAnimal( jObject, "Type" ) == "Cat" )
+                    return null;
+
+                throw new InvalidOperationException();
+            }
+        }
     }
+
+
 }
