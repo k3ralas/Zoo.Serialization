9884663b0622b579fd9d7adc8f6e081b477acf28
fcuchot <cuchot@intechinfo.fr>
2018-03-12 14:09:00 +0100

    Implemented Json Deserialization

--- ZooRunner/ZooSample.Tests/ZooSample.Tests.cs
+++ ZooRunner/ZooSample.Tests/ZooSample.Tests.cs
@@ -1,4 +1,4 @@
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -38,46 +38,15 @@ namespace ZooSample.Tests
             z.Find<Bird>( "Titi" ).Should().NotBeNull();
        
             JObject o = JObject.FromObject( z.Find<Bird>("Titi" ));
-         
+            o["type"] = typeof( Bird ).Name;
 
             var deserialzation = z.Read( o );
 
 
-            var allAnimals = z.SelectAll<List<KeyValuePair<string, Animal>>>();
-
-           var output = Newtonsoft.Json.JsonConvert.SerializeObject( allAnimals );
-        //JArray array  = JArray.Parse( output );
-            
-        //        JObject o1 = JObject.Parse( array[1].ToString() );
-        //    //o1["Animals"] = JObject.FromObject( allAnimals );
-
-
-   
-        }
-        [Test]
-        public void PetConverter()
-        {
-            Zoo z = new Zoo();
-            z.CreateBird( "Titi" );
            
-
-            var json = JsonConvert.SerializeObject( z.Find<Bird>( "Titi" ) );
-
-            var converter = new Zoo.PetConverter();
-            var deserializedArray = JsonConvert.DeserializeObject<Animal>(
-                json,
-                converter );
-
-            
-
-            //for( var i = 0; i < originalArray.Length; i++ )
-            //{
-            //    var original = originalArray[i];
-            //    var deserialized = deserializedArray[i];
-
-            //    Assert.Equal( original.GetType(), deserialized.GetType() );
-            //    Assert.Equal( original.Name, deserialized.Name );
-            //}
+   
+        
+      
         }
     }
 }
--- ZooRunner/ZooSample/Zoo.cs
+++ ZooRunner/ZooSample/Zoo.cs
@@ -1,4 +1,4 @@
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
@@ -47,7 +47,7 @@ namespace ZooSample
         public Cat CreateCat( string name )
         {
             Cat cat = new Cat( this, name );
-            _animals[ name ] = cat;
+            _animals[name] = cat;
             return cat;
         }
 
@@ -62,7 +62,7 @@ namespace ZooSample
         public Bird CreateBird( string name )
         {
             Bird bird = new Bird( this, name );
-            _animals[ name ] = bird;
+            _animals[name] = bird;
             return bird;
         }
 
@@ -95,7 +95,7 @@ namespace ZooSample
             var sinX = Math.Sin( x ) + 1.0;
             var sinY = Math.Sin( y ) + 1.0;
             var sinXY = Math.Sin( x * y ) + 1.0;
-            return Color.FromArgb( (int)(sinX*255) % 256, (int)(sinY*200) % 256, (int)(sinXY * 250) % 256 );
+            return Color.FromArgb( (int)(sinX * 255) % 256, (int)(sinY * 200) % 256, (int)(sinXY * 250) % 256 );
         }
 
         public Animal Read( BinaryReader r )
@@ -107,109 +107,24 @@ namespace ZooSample
         {
             throw new NotImplementedException();
         }
-        public T SelectAll<T>(  ) where T : List<KeyValuePair<string, Animal>>
-        {
-            var list = new List<KeyValuePair<string, Animal>>();
-           
-            var a = _animals.ToList();
-            foreach(var z in a )
-            {
-                list.Add( z );
-            }
-            return list as T;
-        }
+
+
         public Animal Read( JObject o )
         {
+            var type = (string)o["type"];
+            var methodName = $"Create{type}";
+            var name = (string)o["Name"];
 
-            string typeName = (o["Type"]).ToString();
-            switch( typeName )
-            {
-                case "Bird":
-                    
-                   
-                   Bird b2 =  o.ToObject<Bird>();
-                    return b2;
-                case "Cat":
-                    return new Cat( this, o["Name"].ToString() );
-                default:
-                    return null;
-            }
+            var method = typeof( Zoo ).GetMethod( methodName );
 
-            var a = (Bird)System.Reflection.Assembly.GetExecutingAssembly().CreateInstance("Bird" );
-            var arr = new object[] { this, "tott" };
-            Animal b = (Animal)Activator.CreateInstance(typeof(Bird), arr);
-            
-            //_animals[obj.Name] = obj;
+            Animal a = (Animal)method.Invoke( this, new object[] { name } );
+            Type t = Type.GetType( typeof( Animal ).Namespace + "." + type );
 
-
-
-            List<string> keys = o.Properties().Select( p => p.Name ).ToList();
-            List<JToken> values = new List<JToken>();
-
-            var props = typeof( Animal ).GetProperties();
-            
-            foreach( string k in keys )
-            {
-               
-              props.Where( p => p.Name == k ).FirstOrDefault().SetValue(b, o.GetValue(k));
-                
-
-                //var value = o.GetValue( k );
-                //values.Add( value );
-            }
-            
-            return null;
+            t.GetMethod( nameof( Bird.MoveTo ) ).Invoke( a, new object[] { new Point( (double)o["X"], (double)o["Y"] ), 1 } );
+            return a;
         }
-        public abstract class AbstractJsonConverter<T> : JsonConverter
-        {
-            protected abstract T Create( Type objectType, JObject jObject );
-
-            public override bool CanConvert( Type objectType )
-            {
-                return typeof( T ).IsAssignableFrom( objectType );
-            }
-
-            public override object ReadJson(
-                JsonReader reader,
-                Type objectType,
-                object existingValue,
-                JsonSerializer serializer )
-            {
-                var jObject = JObject.Load( reader );
-
-                T target = Create( objectType, jObject );
-                serializer.Populate( jObject.CreateReader(), target );
-
-                return target;
-            }
-
-            public override void WriteJson(
-                JsonWriter writer,
-                object value,
-                JsonSerializer serializer )
-            {
-                throw new NotImplementedException();
-            }
-
-            protected static string TypeOfAnimal(JObject jObject, string name)
-            {          
-                return jObject.GetValue( name ).ToString();
-            }
-        }
-
-        public class PetConverter : AbstractJsonConverter<Animal>
-        {
-            protected override Animal Create( Type objectType, JObject jObject )
-            {
-                if( TypeOfAnimal( jObject, "Type" ) == "Bird")
-                    return null;
-                if( TypeOfAnimal( jObject, "Type" ) == "Cat" )
-                    return null;
-
-                throw new InvalidOperationException();
-            }
-        }
+    }
+        
     }
 
 
-}
